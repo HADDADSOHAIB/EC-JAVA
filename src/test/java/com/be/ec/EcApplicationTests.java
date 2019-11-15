e0d09cdb41138847cfb2f9b991731e6a6e0ea673
HADDADSOHAIB <souhaib.haddad@gmail.com>
2019-11-15 11:26:53 +0100

    initial files

---
+++ src/test/java/com/be/ec/EcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
